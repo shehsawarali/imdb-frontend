2354529101a26cac5aa17a19892b4a343222fa45
shehsawarali <shehsawar.ali@arbisoft.com>
2021-08-25 14:51:17 +0500

    updated Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,29 @@
 import React from "react";
-import {Navbar, Nav} from "react-bootstrap";
+import {Navbar, Nav, Button} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 const AppNavbar = () => {
   return (
-    <Navbar bg="dark" variant="dark" sticky={"top"} style={{padding: "0.5rem 1rem"}}>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="me-auto">
-        <Link to="/">Home</Link>
-        <Link to="/features">Features</Link>
-        <Link to="/pricing">Pricing</Link>
-      </Nav>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      sticky={"top"}
+      style={{padding: "0.5rem 1rem"}}
+    >
+      <Navbar.Brand as={Link} to="/">
+        Navbar
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto"></Nav>
+
+        <Nav justify={"space-between"}>
+          <Button variant={"primary"}>Sign Up</Button>
+          <Button variant={"warning"}>Sign In</Button>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
