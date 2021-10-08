cd3eb5f37f8bde1175241e18e078330decc4751c
shehsawarali <shehsawar.ali@arbisoft.com>
2021-10-08 17:25:22 +0500

    changed BACKEND_URL for production

--- src/constant.js
+++ src/constant.js
@@ -1,7 +1,7 @@
 export const EMAIL_TEST_REGEX =
   /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b/;
 
-export const BACKEND_URL = "http://localhost:8000";
+export const BACKEND_URL = "https://filmfilia-server.herokuapp.com";
 export const JWT_PREFIX = "Bearer";
 export const REFRESH_TOKEN = "refresh_token";
 export const ACCESS_TOKEN = "access_token";
