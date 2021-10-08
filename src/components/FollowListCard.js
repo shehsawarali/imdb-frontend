dd7656312214f2ac444dd89bb8be11696e726242
shehsawarali <shehsawar.ali@arbisoft.com>
2021-10-09 01:35:45 +0500

    Update FollowListCard
    - Increased width of information div in FollowListCard

--- src/components/FollowListCard.js
+++ src/components/FollowListCard.js
@@ -17,7 +17,7 @@ const FollowListCard = ({ user }) => {
             alt={"profile"}
             style={{ height: "70px", width: "70px" }}
           />
-          <div className={"text-overflow w-50 lightgray"}>
+          <div className={"text-overflow w-100 lightgray"}>
             {user.first_name} {user.last_name}
             <br />
             {user.email}
